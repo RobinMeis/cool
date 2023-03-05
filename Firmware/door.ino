74b987b78091fbbaa696311674500751ed43f02b
Robin Meis <robin.meis@smartnoob.de>
2023-03-06 00:06:56 +0100

    Fix door debounce

--- Firmware/door.ino
+++ Firmware/door.ino
@@ -2,25 +2,29 @@ bool door_is_open;
 unsigned long door_debounce_start=0;
 
 void door_init() {
-  pinMode(PIN_DOOR, INPUT_PULLUP);
-  if (digitalRead(PIN_DOOR) == !PIN_DOOR_ACTIVE_LOW)
-    door_open();
-  else
-    door_closed();
+  pinMode(PIN_DOOR, INPUT);
+  door_read(true);
 }
 
-void door_open() {
-  if (door_is_open || millis() - door_debounce_start > DOOR_DEBOUNCE_TIME)
-    return;
+void door_read(bool force_update) {
+  if (digitalRead(PIN_DOOR) == !PIN_DOOR_ACTIVE_LOW)
+    door_open(force_update);
+  else
+    door_closed(force_update);
+}
 
+void door_open(bool force_update) {
+  if (!force_update && (door_is_open || millis() - door_debounce_start < DOOR_DEBOUNCE_TIME))
+    return;
+  
   door_debounce_start = millis();
   door_is_open = true;
   light_on();
   door_status();
 }
 
-void door_closed() {
-  if (!door_is_open || millis() - door_debounce_start > DOOR_DEBOUNCE_TIME)
+void door_closed(bool force_update) {
+  if (!force_update && (!door_is_open || millis() - door_debounce_start < DOOR_DEBOUNCE_TIME))
     return;
 
   door_debounce_start = millis();
@@ -37,9 +41,5 @@ void door_status() {
 }
 
 void door_loop() {
-  Serial.println(digitalRead(PIN_DOOR));
-  if (digitalRead(PIN_DOOR) == !PIN_DOOR_ACTIVE_LOW)
-    door_open();
-  else
-    door_closed();
+  door_read(false);
 }
